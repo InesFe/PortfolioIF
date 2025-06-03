f9b77d9e42bfd3902efff12220d694d483fe438c
Inès Fe <ines.fevrier@gmail.com>
2025-06-03 11:42:24 +0200

    almost done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @projects = [
+      { name: "FUN WITH LAW", url: "#", year: "2025", tech: "BackEnd+FrontEnd" }
+    ]
   end
 
   def diner
+    @projects = [
+      { name: "FUN WITH LAW", url: "#", year: "2025", tech: "BackEnd+FrontEnd" }
+    ]
   end
 end
